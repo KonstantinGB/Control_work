4cf08891ec76552ad86ef528ffbb8d9b72904033
Konstantin <osipo38@yandex.ru>
2022-09-09 14:26:04 +0300

    added program

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿string[] LessThanThreeCharacters ( string[] array)
+{
+    int count = 0;
+    for( int i =0; i < array.Length; i ++)
+    {
+        if(array[i].Length <= 3) count ++; 
+    }
+
+    string [] newArray = new string[count];
+
+    for(int i = 0, k = 0; i < array.Length; i ++)
+    {
+        if( array[i].Length <= 3)
+        {
+            newArray[k] = array[i];
+            k++;
+        }
+    }
+    return newArray;
+
+}
+
+void ShowArray(string[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+
+string[] array = {"1234", "1567", "-2", "computer science"};
+
+string[] newArray = LessThanThreeCharacters(array);
+ShowArray(newArray);
